b3b7351cfad60c4dac56200f0178a0cc09722af3
techno-tanoC <develop.tanoc@gmail.com>
2019-09-18 20:44:38 +0900

    add vscode snippet

--- setup.sh
+++ setup.sh
@@ -30,6 +30,7 @@ function link_code() {
     ln -s ~/dotfiles/vscode/settings.json ~/.config/Code/User/settings.json
   elif [ `lsb_release -is` = "Ubuntu" ] ; then
     ln -s ~/dotfiles/vscode/settings.json ~/.config/Code/User/settings.json
+    ln -s "~/dotfiles/vscode/snippets" "~/Library/Application Support/Code/User/snippets"
   else
     echo "unsupported"
     exit 1

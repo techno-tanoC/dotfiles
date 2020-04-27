a7c7ce4f065f3809633605b2ad60f49da87d5ad2
techno-tanoC <develop.tanoc@gmail.com>
2020-04-27 13:21:33 +0900

    fix asdf

--- zsh/lang.sh
+++ zsh/lang.sh
@@ -1,9 +1,11 @@
 # asdf
 if [ -f $HOME/.asdf/asdf.sh ]; then
   . $HOME/.asdf/asdf.sh
-fi
-if [ -f $HOME/.asdf/completions/asdf.bash ]; then
-  . $HOME/.asdf/completions/asdf.bash
+  # append completions to fpath
+  fpath=(${ASDF_DIR}/completions $fpath)
+  # initialise completions with ZSH's compinit
+  autoload -Uz compinit
+  compinit
 fi
 
 # ruby

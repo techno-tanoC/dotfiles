604d7e2653721bd13db74a78c97a26bd2f4859f3
techno-tanoC <develop.tanoc@gmail.com>
2021-05-27 21:00:26 +0900

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@
 
 # git
 mkdir -p ~/.config/git/
-ln -s ~/dotfiles/git/ignore ~/.config/git/ignore
-ln -s ~/dotfiles/git/.gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/git/ignore ~/.config/git/ignore
+ln -sf ~/dotfiles/git/.gitconfig ~/.gitconfig
 
 # zsh
 ln -sf ~/dotfiles/zsh/zshrc ~/.zshrc
@@ -15,9 +15,9 @@ curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh >
 sh ./installer.sh ~/.vim/repos
 rm installer.sh
 
-ln -s ~/dotfiles/vim/.vimrc ~/.vimrc
-ln -s ~/dotfiles/vim/dein.toml ~/.vim/dein.toml
-ln -s ~/dotfiles/vim/dein_lazy.toml ~/.vim/dein_lazy.toml
+ln -sf ~/dotfiles/vim/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/vim/dein.toml ~/.vim/dein.toml
+ln -sf ~/dotfiles/vim/dein_lazy.toml ~/.vim/dein_lazy.toml
 
 # vlc
 ln -sf ~/dotfiles/vlc/vlcrc ~/.config/vlc/vlcrc
@@ -25,13 +25,13 @@ ln -sf ~/dotfiles/vlc/vlcrc ~/.config/vlc/vlcrc
 # code
 link_code() {
   if [ `uname` = "Darwin" ] ; then
-    ln -s ~/dotfiles/vscode/settings.json ~/Library/Application Support/Code/User/settings.json
+    ln -sf ~/dotfiles/vscode/settings.json ~/Library/Application Support/Code/User/settings.json
   elif [ `lsb_release -is` = "ManjaroLinux" ] ; then
-    ln -s ~/dotfiles/vscode/settings.json ~/.config/Code/User/settings.json
-    ln -s ~/dotfiles/vscode/snippets ~/.config/Code/User/snippets
+    ln -sf ~/dotfiles/vscode/settings.json ~/.config/Code/User/settings.json
+    ln -sf ~/dotfiles/vscode/snippets ~/.config/Code/User/snippets
   elif [ `lsb_release -is` = "Ubuntu" ] ; then
-    ln -s ~/dotfiles/vscode/settings.json ~/.config/Code/User/settings.json
-    ln -s ~/dotfiles/vscode/snippets ~/.config/Code/User/snippets
+    ln -sf ~/dotfiles/vscode/settings.json ~/.config/Code/User/settings.json
+    ln -sf ~/dotfiles/vscode/snippets ~/.config/Code/User/snippets
   else
     echo "unsupported"
     exit 1

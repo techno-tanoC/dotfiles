90e73ea6d59a1093b40f4bcff956e85f5120b621
techno-tanoC <develop.tanoc@gmail.com>
2024-01-07 21:58:40 +0900

    Add vim files

--- setup.sh
+++ setup.sh
@@ -8,6 +8,18 @@ ln -sf ~/dotfiles/git/gitconfig ~/.gitconfig
 # zsh
 ln -sf ~/dotfiles/zsh/zshrc ~/.zshrc
 
+# vim
+install_plugins() {
+  curl https://raw.githubusercontent.com/tomasr/molokai/master/colors/molokai.vim > ~/.vim/colors/molokai.vim
+  git clone https://github.com/preservim/nerdtree.git ~/.vim/pack/vendor/start/nerdtree
+  git clone https://github.com/tomtom/tcomment_vim ~/.vim/pack/tml/start/tcomment_vim
+  git clone https://github.com/itchyny/lightline.vim ~/.vim/pack/plugins/start/lightline
+  git clone https://github.com/Yggdroot/indentLine.git ~/.vim/pack/vendor/start/indentLine
+}
+install_plugins
+ln -sf ~/dotfiles/vim/vimrc ~/.vimrc
+ln -sf ~/dotfiles/vim/ideavimrc ~/.ideavimrc
+
 # vlc
 ln -sf ~/dotfiles/vlc/vlcrc ~/.config/vlc/vlcrc
 

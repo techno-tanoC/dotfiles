9df1750a25dc1978377e81f7789de78bf4519677
techno-tanoC <develop.tanoc@gmail.com>
2025-06-11 14:40:19 +0900

    Update devcontainer installer

--- devcontainer/install.sh
+++ devcontainer/install.sh
@@ -13,3 +13,6 @@ alias pu="pulumi up --refresh --diff"
 alias pp="pulumi preview --refresh --diff"
 alias pi="pulumi import --protect=false --yes"
 EOF
+
+mkdir -p ~/.config/git/
+ln -sf ~/dotfiles/git/gitignore ~/.config/git/ignore
